df9ab1566acd34734d793e6fd023f204765151b1
Mustafa Kilic <mstfklc16@gmail.com>
2024-10-20 17:21:11 +0300

    summary add

--- src/admin/summary/summary.service.ts
+++ src/admin/summary/summary.service.ts
@@ -1,4 +1,29 @@
 import { Injectable } from '@nestjs/common';
+import { InjectModel } from '@nestjs/mongoose';
+import mongoose from 'mongoose';
+import { Company } from '../../schemas/company.schema';
+import { User } from '../../schemas/user.schema';
+import { SummaryResponseDto } from './dto/response/summaryResponse.dto';
+import { AuthRequestDto } from '../../custom/jwt/dto/auth.request.dto';
 
 @Injectable()
-export class SummaryService {}
+export class SummaryService {
+  constructor(
+    @InjectModel(Company.name)
+    private companyModel: mongoose.Model<Company>,
+    @InjectModel(User.name)
+    private userModel: mongoose.Model<User>,
+  ) {}
+
+  // eslint-disable-next-line @typescript-eslint/no-unused-vars
+  async getSummaryData(auth: AuthRequestDto): Promise<SummaryResponseDto> {
+    const companyCount = await this.companyModel.countDocuments();
+    const userCount = await this.userModel.countDocuments();
+    return {
+      totalPlayedSongs: 0,
+      totalEarningPrice: 0,
+      totalCompanyCount: companyCount,
+      totalUserCount: userCount,
+    };
+  }
+}
